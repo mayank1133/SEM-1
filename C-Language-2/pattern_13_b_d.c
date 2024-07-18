90b36d32a8fda5c747be1d00d873bd8907a0af7f
mayank1133 <140825299+mayank1133@users.noreply.github.com>
2024-07-18 11:14:08 +0530

    Add files via upload

---
+++ C-Language-2/pattern_13_b_d.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+	int r,c,n,num,a;
+	char ch='A';
+	printf("Enter Ending value for row : ");
+	scanf("%d",&n);
+
+    for(r=1;r<=n;r++){
+        for(a=n;a>=r;a--){
+            printf(" ");
+        }
+        num=1;
+        for(c=1;c<=r;c++){
+        	if(c!=1){
+        		printf(" ");
+			}
+			if(r%2!=0){
+				printf("%d",num++);
+			}
+			else{
+				printf("%c",ch++);
+			}
+        }
+         printf("\n");
+    }
+
+}
---
+++ C-Language-2/user_math_cal.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+void main(){
+	int a,b;
+    char c;
+
+	printf("Enter value : ");
+	scanf("%d",&a);
+    printf("Enter Arithmatic Sign : ");
+	    scanf(" %c",&c);
+	while(c!='0'){
+         printf("Enter next value : ");
+	        scanf("%d",&b);
+        switch(c){
+		case '+':
+        a=(a+b);
+			printf("Answer = %d\n",(a));
+			break;
+		case '-':
+			a=(a-b);
+			printf("Answer = %d\n",(a));
+			break;
+		case '*':
+			a=(a*b);
+			printf("Answer = %d\n",(a));
+			break;
+		case '/':
+			if(b==0){
+			printf("Not Defined\n");
+            printf("Promgram Restart\n");
+            printf("Enter value : ");
+	        scanf("%d",&a);
+		}
+		    else{
+			a=(a/b);
+			printf("Answer = %d\n",(a));
+		}
+			break;
+		case '%':	
+			a=(a%b);
+			printf("Answer = %d\n",(a));
+			break;
+        case '=':	
+			printf("Answer = %d\n",(a));
+			break;
+		default:
+             c='0';
+			printf("Invalid Invalid Sign For Mathematical calculation");
+			break;
+	}
+    if(c!=0){
+    printf("Enter number for Mathematical calculation : ");
+	    scanf(" %c",&c);
+    }
+    }
+	
+}
